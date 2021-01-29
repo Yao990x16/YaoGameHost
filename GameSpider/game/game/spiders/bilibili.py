b0c17378148c33f38c1f1bf0f3236379dc97eda0
Yao990x16 <839171621@qq.com>
2021-01-29 21:38:03 +0800

    scrapy,随机更换请求头(pip install fake-useragent)

--- GameSpider/game/game/spiders/bilibili.py
+++ GameSpider/game/game/spiders/bilibili.py
@@ -3,11 +3,11 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy_redis.spiders import RedisCrawlSpider
 
-class BilibiliSpider(RedisCrawlSpider):
+class BilibiliSpider(CrawlSpider):
     name = 'bilibili'
     allowed_domains = ['bilibili.com']
-    #start_urls = ['http://bilibili.com/']
-    redis_key = 'bilibili'
+    start_urls = ['https://www.bilibili.com/v/game/match/schedule?mid=0&gid=0&tid=0&time=1611878400000']
+    #redis_key = 'bilibili'
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
