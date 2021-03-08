43f340246b6469594923b47ddbc2b6eb8176a0e8
Yao990x16 <839171621@qq.com>
2021-03-08 13:46:21 +0800

    将赛事id保存到本地txt,获取比赛信息时候通过读取本地txt来获得规定时间内所有比赛的信息

--- GameSpider/game/game/spiders/txty.py
+++ GameSpider/game/game/spiders/txty.py
@@ -1,16 +1,27 @@
+import os
 import time
 import json
+from typing import TextIO
+
 from scrapy import Request
 import scrapy
 from ..items import TxtySportTypeItem
 from ..items import TxtySportTimeItem
+
+
+# noinspection PyGlobalUndefined,PyRedeclaration
 class TxtySpider(scrapy.Spider):
     name = 'txty'
     allowed_domains = ['qq.com']
     #体育类型
     sports_typeUrl = 'https://matchweb.sports.qq.com/matchUnion/cateColumns?from=pc'
     #赛事信息
-    sports_timeUrl = 'https://matchweb.sports.qq.com/matchUnion/list?today={today}&startTime={startTime}&endTime={endTime}&columnId=100000&index=3&isInit=true'
+    sports_timeUrl = 'https://matchweb.sports.qq.com/matchUnion/list?today={today}&startTime={' \
+                     'startTime}&endTime={endTime}&columnId={columnID}&index=3&isInit=true'
+    #当前路径
+    global currentPath
+    # noinspection PyRedeclaration
+    currentPath = os.path.abspath(os.path.dirname(__file__))
 
     def parse(self, response, **kwargs):
         pass
@@ -24,24 +35,34 @@ class TxtySpider(scrapy.Spider):
             #爬取原始数据
             stime_str = input('请输入开始日期(xxxx-xx-xx):')
             etime_str = input('请输入结束日期(xxxx-xx-xx):')
-            yield Request(self.sports_timeUrl.format(today=localdate,
-                                                     startTime=stime_str,
-                                                     endTime=etime_str),
-                          callback=self.parse_sportsTime)
+            file = open(currentPath+"\\columnID.txt", 'r+')
+            for columnID in file:
+                yield Request(self.sports_timeUrl.format(today=localdate,
+                                                         startTime=stime_str,
+                                                         endTime=etime_str,
+                                                         columnID=columnID),
+                              callback=self.parse_sportsTime)
         elif request_code == '3':
             #%Y四位数年份表示,%m月份,%d月内中的一天
             #获取当天日期,爬取当天比分变化
             print('今天的日期:', localdate)
-            yield Request(self.sports_timeUrl.format(today=localdate,
-                                                     startTime=localdate,
-                                                     endTime=localdate),
-                          callback=self.parse_sportsTime)
+            file = open(currentPath+"\\columnID.txt", 'r+')
+            for columnID in file:
+                yield Request(self.sports_timeUrl.format(today=localdate,
+                                                         startTime=localdate,
+                                                         endTime=localdate,
+                                                         columnID=columnID),
+                              callback=self.parse_sportsTime)
         else:
             print('请输入1或2或3')
 
     @staticmethod
     def parse_sportsType(response):
         result = json.loads(response.text)
+        print("当前目录:"+currentPath)
+        filePath = currentPath+"\\columnID.txt"
+        if os.path.exists(filePath):
+            os.remove(filePath)
         if result.get('data'):
             sports = result['data']
             for sport in sports:
@@ -52,12 +73,15 @@ class TxtySpider(scrapy.Spider):
                     icon = column.get('icon')
                     name = column.get('name')
                     matchNum = column.get('matchNum')
+                    fd = open(filePath, 'a')
+                    fd.write(column_ID+'\n')
                     item = TxtySportTypeItem(sport_title=sport_title,
                                              column_ID=column_ID,
                                              icon=icon,
                                              name=name,
                                              matchNum=matchNum)
                     yield item
+                fd.close()
 
     @staticmethod
     def parse_sportsTime(response):
