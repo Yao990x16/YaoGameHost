af303640659041bca94a57fd5f6360664e637058
Yao990x16 <839171621@qq.com>
2021-04-20 22:38:28 +0800

    预测回归模型初步搭建,正确率在0.66左右

---
+++ EventForecast/NBAForecast/result.py
@@ -0,0 +1,22 @@
+import csv
+import pandas as pd
+
+October_2019 = pd.read_csv('data/October_2019.csv')
+November_2019 = pd.read_csv('data/November_2019.csv')
+December_2019 = pd.read_csv('data/December_2019.csv')
+January_2020 = pd.read_csv('data/January_2020.csv')
+February_2020 = pd.read_csv('data/February_2020.csv')
+March_2020 = pd.read_csv('data/March_2020.csv')
+July_2020 = pd.read_csv('data/July_2020.csv')
+August_2020 = pd.read_csv('data/August_2020.csv')
+September_2020 = pd.read_csv('data/September_2020.csv')
+October_2020 = pd.read_csv('data/October_2020.csv')
+December_2020 = pd.read_csv('data/December_2020.csv')
+January_2021 = pd.read_csv('data/January_2021.csv')
+February_2021 = pd.read_csv('data/February_2021.csv')
+March_2021 = pd.read_csv('data/March_2021.csv')
+April_2021 = pd.read_csv('data/April_2021.csv')
+May_2021 = pd.read_csv('data/May_2021.csv')
+
+def get19_20result():
+ 	pass
